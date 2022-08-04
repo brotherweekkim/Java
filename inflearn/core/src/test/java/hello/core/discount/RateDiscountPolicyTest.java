c97367f8101e7200eb50a7d15b5db5dfec6fb7d8
Hyung Ju <ghj6282@hanmail.net>
2022-08-04 21:25:19 +0900

    0804 Java TIL

---
+++ inflearn/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,24 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_0() {
+        //given
+        Member member = new Member(1L, "mem", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+    }
+
+}
